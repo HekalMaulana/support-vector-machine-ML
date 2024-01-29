80d03ad7ac4aefc1728f486b973b593420cc122f
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-29 17:50:53 +0800

    Splitting dataset into training set and test set and apllying feature scaling

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+# Splitting dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.25)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+print(X_train)
+print(X_test)
