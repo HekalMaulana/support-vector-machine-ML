58294fe7c0cd9a39705da903dc2dcb20f7335364
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-29 20:06:31 +0800

    Training SVM model, predict the result, and catch accuracy score

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, :-1].values
@@ -16,5 +18,17 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-print(X_train)
-print(X_test)
+# Training SVM Model
+classifier = SVC(kernel='linear', random_state=0)
+classifier.fit(X_train, y_train)    
+
+# Predict Training set Result
+print(classifier.predict(sc.transform([[30, 87000]])))
+
+# Predicting Test Set Result
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+# Accuracy Score
+print(accuracy_score(y_true=y_test, y_pred=y_pred))
+print(confusion_matrix(y_true=y_test, y_pred=y_pred))
